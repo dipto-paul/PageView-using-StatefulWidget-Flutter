6e71780c5a1e06c5d4910405b63eeb4f4fb6563b
Dipto Paul <diptopaul.contact@gmail.com>
2026-07-25 22:13:17 +0600

    PageView widget added

---
+++ lib/page1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class page1 extends StatelessWidget {
+  const page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.red,
+        ),
+      )
+    );
+  }
+}
---
+++ lib/page3.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class page3 extends StatelessWidget {
+  const page3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body: Container(
+            color: Colors.blue,
+          ),
+        )
+    );
+  }
+}
