e5485c65b2a8bf206dc5ac34250af5859d09920f
Dipto Paul <diptopaul.contact@gmail.com>
2026-07-26 13:23:26 +0600

    Listile added in Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -28,13 +28,24 @@ class _MyAppState extends State<MyApp> {
             children: [
               DrawerHeader(child: Text("Navigation Drawer",style: TextStyle(color:Colors.black,fontSize: 20,fontWeight: FontWeight.bold)),
               ),
+              Padding(padding: EdgeInsets.all(10)),
+              ListTile(
+                leading: Icon(Icons.home,), title: Text("Home"),
+              ),
+
+              ListTile(
+                leading: Icon(Icons.settings,), title: Text("Settings"),
+              ),
+              ListTile(
+                leading: Icon(Icons.contact_emergency,), title: Text("Contact us"),
+              ),
 
             ],
           )
 
         ),
         appBar: AppBar(
-          title: Text("hello!"),
+          title: Text("Scroll Left <---"),
           backgroundColor: Colors.blue,
         ),
         body: PageView(
